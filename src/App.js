d31b9fd9ae84f9dfec248fa6547b5debcac8d590
John Emerson <john.carv.sousa@gmail.com>
2022-02-03 09:04:01 -0300

    feat: create task list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import TaskInput from './TaskInput';
-// import TaskList from './TaskList';
+import TaskList from './TaskList';
 
 function App() {
   return (
     <div className="App">
       <TaskInput />
-      {/* <TaskList /> */}
+      <TaskList />
     </div>
   );
 }
---
+++ src/TaskList.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react'
+import {collection, query, orderBy, onSnapshot} from "firebase/firestore"
+import {db} from './firebase'
+
+function TaskList() {
+
+  const [tasksLoading, setTasksLoading] = useState(true)
+  const [tasks, setTasks] = useState([])
+
+  /* function to get all tasks from firestore in realtime */ 
+  useEffect(() => {
+    const taskColRef = query(collection(db, 'tasks'), orderBy('created', 'desc'))
+    onSnapshot(taskColRef, (snapshot) => {
+      setTasks(snapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+    })
+    setTasksLoading(false)
+  },[])
+
+  let taskList;
+  if (tasksLoading) {
+    taskList = <div className="TaskList-empty">Loading...</div>;
+  } else if (tasks.length) {
+    taskList = (
+      <ul className="TaskList">
+        {tasks.map(task => (
+          <h4 key={task.id}>{task.data.text}</h4>
+        ))}
+      </ul>
+    );
+  } else {
+    taskList = <div className="TaskList-empty">No Tasks</div>;
+  }
+
+  return (
+    <>{taskList}</>
+  )
+}
+
+export default TaskList
