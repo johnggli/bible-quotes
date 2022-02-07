c4a2626daa837b0c025c39d21a221d77128a153c
John Emerson <john.carv.sousa@gmail.com>
2022-02-07 12:55:46 -0300

    fix: footer error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,9 @@
-import { Link } from 'react-router-dom'
-
 function Footer() {
   return (
     <div className='footer'>
       <div>
         Made with <i className='fa fa-heart'></i> by
-        <a href='https://johnemerson1406.github.io/linktree/' target='_blank'>@JohnEmerson1406</a>
+        <a href='https://johnemerson1406.github.io/linktree/' target='_blank' rel='noopener noreferrer'>@JohnEmerson1406</a>
       </div>
     </div>
   )
