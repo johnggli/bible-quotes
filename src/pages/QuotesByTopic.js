009384ecffaf9f41694118f0f0dae01efba0bf75
John Emerson <john.carv.sousa@gmail.com>
2022-02-28 21:01:09 -0300

    style: change opacity of footer link

--- src/pages/QuotesByTopic.js
+++ src/pages/QuotesByTopic.js
@@ -5,7 +5,7 @@ function QuotesByTopic(props) {
   return (
     <div className='container'>
       <div className='content'>
-        <Banner title={`${props.topic}`} image='https://user-images.githubusercontent.com/43749971/155612677-c28e0066-b82a-4713-8ebb-75e1cc273400.png'/>
+        <Banner title={props.topic} image='https://user-images.githubusercontent.com/43749971/155612677-c28e0066-b82a-4713-8ebb-75e1cc273400.png'/>
         <QuoteList topic={props.topic}/>
       </div>
     </div>
